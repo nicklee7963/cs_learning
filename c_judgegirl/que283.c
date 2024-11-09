8845c73281f9b827c04a8be026161d0c6cd65242
nicklee <nicklee7963@gmail.com>
2024-11-09 11:57:15 +0800

    first day

--- c_judgegirl/que283.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
-int main(){
-    int s, f, t;
-    scanf("%d%d%d", &s, &f, &t);
-    int chicken, rabbit, crab;
-    crab = s-t;
-    
-}
--- c_judgegirl/que3.c
+++ c_judgegirl/que3.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 int main(){
-    int i, j;
-
-    scanf("%d", &i);
-    scanf("%d", &j);
-
-    printf("%d\n", j);
-    printf("%d", i);
-
-
-
+    int a,b;
+    scanf("%d%d", &a, &b);
+    printf("%d\n%d",b,a);
+    return 0;
 }
